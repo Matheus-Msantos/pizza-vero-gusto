67e7c5f88983bdbdb28f729a827a5b8a301996cc
Matheus-Msantos <matheusmelosantos10@gmail.com>
2024-03-04 20:46:26 -0300

    create structure

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,9 @@
-import { Router, Response, Request } from 'express';
+import { Router } from 'express';
+import { CreateUserController } from './controllers/user/CreateUserController';
 
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
-  return res.json({ ok: true });
-});
+// --- ROTAS USERS ---
+router.post('/users', new CreateUserController().handle)
 
 export { router };
